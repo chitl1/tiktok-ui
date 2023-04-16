1bf8820b0f95a46ca0351307c12eaae16e068bab
Chi <thanlinhchi010995.dhcn@gmail.com>
2023-04-17 00:23:31 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layout';
 import { Fragment } from 'react';
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route,index) =>{
-                        //const Layout = route.layout === null ? Fragment :  DefaultLayout;
-                        const Page = route.component;
-                        let Layout = DefaultLayout;
-                        if(route.layout){
-                            Layout = route.layout
-                            
-                        }
-                        else if(route.layout === null){
-                            Layout = Fragment
-                        }
-                        else{
-                            
-                        }
-        
-                        return <Route key={index} path={route.path} element={<Layout><Page/></Layout>} />
-                    })}
-                    {/* <Route path="/" element={<Home/>}></Route>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            //const Layout = route.layout === null ? Fragment :  DefaultLayout;
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            } else {
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+          {/* <Route path="/" element={<Home/>}></Route>
                     <Route path="/following" element={<Following/>}></Route> */}
-                </Routes>
-            </div>
-        </Router>
-    );
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,16 +1,16 @@
-import Sidebar from "./Sidebar";
-import Header from "../components/Header";
+import Sidebar from './Sidebar';
+import Header from '../components/Header';
 
-function DefaultLayout({children}) {
-    return ( <div>
-        <Header/>
-        <div class='container'>
-            <Sidebar/>
-            <div className="content">
-                {children}
-            </div>
-        </div>
-    </div> );
+function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <div class="container">
+        <Sidebar />
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default DefaultLayout;
