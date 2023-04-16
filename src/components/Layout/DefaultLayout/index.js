40d703331e51d3514c338137d6849a04f8c855d8
Chi <thanlinhchi010995.dhcn@gmail.com>
2023-04-17 01:17:24 +0700

    baif 96. dung khung layou mac dinh

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,16 @@
+import classNames from 'classnames/bind';
 import Sidebar from './Sidebar';
 import Header from '../components/Header';
+import styles from './DefaultLayout.module.scss';
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div class="container">
+      <div className={cx('container')}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,14 +1,14 @@
-import Header from "../components/Header";
+import Header from '../components/Header';
 
-function HeaderOnly({children}) {
-    return ( <div>
-        <Header/>
-        <div class='container'>
-            <div className="content">
-                {children}
-            </div>
-        </div>
-    </div> );
+function HeaderOnly({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="content">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderOnly;
